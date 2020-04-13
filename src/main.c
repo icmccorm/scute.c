b0233570888a99a1ea96e01d8943e5820f967c0f
Ian C. McCormack <mccormic2812@uwec.edu>
2020-04-13 16:20:37 -0500

    Began tests and added support for piping input to the interpreter.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
@@ -50,13 +52,7 @@ static char* readFile(const char* path){
 	return buffer;
 }
 
-static void runFile(const char* path){
-	#ifndef EM_MAIN
-		print(O_OUT, "[A] before init: %d bytes\n", numBytesAllocated);
-	#endif
-
-	char* source = readFile(path);
-
+static InterpretResult runCode(char* source){
 	CompilePackage* compiled = initCompilationPackage();
 	
 	#ifndef EM_MAIN
@@ -74,30 +70,94 @@ static void runFile(const char* path){
 	#ifndef EM_MAIN
 		print(O_OUT, "[A] after fcompile: %d bytes\n", numBytesAllocated);
 	#endif
+
+	return result;
+}
+
+static InterpretResult runFile(const char* path){
+	#ifndef EM_MAIN
+		print(O_OUT, "[A] before init: %d bytes\n", numBytesAllocated);
+	#endif
+	char* source = readFile(path);
+	return runCode(source);
+}
+
+static InterpretResult runInput(){
+	#ifndef EM_MAIN
+		print(O_OUT, "[A] before init: %d bytes\n", numBytesAllocated);
+	#endif
+
+	#define CHUNK (256)
+	char* source = NULL;
+	char* buffer = GROW_ARRAY(NULL, char, 0, CHUNK);
+
+	if(buffer == NULL){
+		print(O_ERR, "Not enough memory to read \"%s\".\n", path);
+		exit(74);
+	}
+
+	FILE* file = stdin;
+	if(file == NULL){
+		print(O_ERR, "Could not receive from standard input.\n");
+		exit(74);
+	}
+
+	int currentSourceLength = 0;
+	int currentBuffLength = 0;
+	do{
+		fgets(buffer, CHUNK, stdin);
+		currentBuffLength = strlen(buffer);
+
+		source = reallocate(source, currentSourceLength, currentSourceLength + currentBuffLength); //void* reallocate(void* previous, size_t oldSize, size_t newSize) {
+		
+		if(buffer == NULL){
+			print(O_ERR, "Not enough memory to read \"%s\".\n", path);
+			exit(74);
+		}
+		
+		strcpy(source + currentSourceLength, buffer);
+
+		currentSourceLength += currentBuffLength;
+
+	}while(currentBuffLength == CHUNK-1 && buffer[CHUNK-2]!='\n');
 	
-	if(result == INTERPRET_COMPILE_ERROR) exit(65);
-	if(result == INTERPRET_RUNTIME_ERROR) exit(70);
+	InterpretResult result = runCode(buffer);
+
+	return result;
 }
 
 int main(int argc, const char* argv[]){
 	const char* sFlag = "-s";
+	InterpretResult result = INTERPRET_OK;
 	switch(argc){
+		case 1: ;
+			if(!isatty(STDIN_FILENO)) {
+				result = runInput();
+			}else{
+				print(O_ERR, "Usage: scute [path] [-s], OR <input> | ./scute\n");
+				exit(64);
+
+			}
+			break;
 		case 2: ;
-			runFile(argv[1]);
+			result = runFile(argv[1]);
 			break;
 		case 3: ;
 			const char* flag = argv[2];
 			if(memcmp(sFlag, flag, 2) == 0){
 				DEBUG_STACK = true;
-				runFile(argv[1]);
+				result = runFile(argv[1]);
 			}else{
-				print(O_ERR, "Usage: scute [path] [-s]\n");
+				print(O_ERR, "Usage: scute [path] [-s], OR <input> | ./scute\n");
+				exit(64);
 			}
 			break;
 		default: ;
-			print(O_ERR, "Usage: scute [path] [-s]\n");
+			print(O_ERR, "Usage: scute [path] [-s], OR <input> | ./scute\n");
 			exit(64);
 			break;
-	}
+	}	
+	if(result == INTERPRET_COMPILE_ERROR) exit(65);
+	if(result == INTERPRET_RUNTIME_ERROR) exit(70);
 	return 0;
 }
--- tests/tests.h
+++ tests/tests.h
@@ -1,6 +1,16 @@
 #ifndef scute_tests_h
 #define scute_tests_h
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
+typedef struct {
+	const char* description;
+} TestSuite;
+
+void assertTrue(const char* that, bool isTrue, const char* orElsePrint);
+
+void assertCharEquals(const char* that, char* stringOne, char* stringTwo, const char* orElsePrint);
 
 #endif
